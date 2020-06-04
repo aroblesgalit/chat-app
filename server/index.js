c249bae38dab67dbee3569881c8d8ac99f1650d0
Alvin Galit <aroblesgalit@gmail.com>
2020-06-04 14:08:09 -0500

    Created index.js in server directory

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
+// Require all necessary dependencies
 const express = require("express");
+const socketio = require("socket.io");
+const http = require("http"); // Built-in module
+
+// Specify our port
+const PORT = process.env.PORT || 5000;
+
+// Set up socket.io
+const app = express(); // Runs our app
+const server = http.createServer(app); // Initialize our server and pass in our app
+const io = socketio(server); // Create io, an instance of socketio and pass in our server
+
+// Run our server 
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
