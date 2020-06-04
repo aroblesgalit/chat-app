78696d0a36a3fafedab8760db67b3af6343ee871
Alvin Galit <aroblesgalit@gmail.com>
2020-06-04 13:49:19 -0500

    Created components director with Join and Chat files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
 function App() {
     return (
