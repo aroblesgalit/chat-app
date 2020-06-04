c353ef2fa6400fc39c021cf2704fce5feaf88713
Alvin Galit <aroblesgalit@gmail.com>
2020-06-04 15:57:15 -0500

    Created helper functin for adding user to chat room

---
+++ users.js
@@ -0,0 +1,36 @@
+// Our array of users
+const users = [];
+
+// Helper functions
+// Add user
+function addUser({ id, name, room }) {
+    // Trim and change casings to lowercase
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+    // Check if there is an existing user that this user is trying to log in with
+    const existingUser = users.find(user => user.room === room && user.name === name);
+
+    if(existingUser) {
+        return { error: "Username is taken" };
+    }
+
+    const user = { id, name, room };
+    users.push(user);
+
+    return { user };
+};
+
+// Remove user
+function removeUser() {
+
+};
+
+// Get user
+function getUser() {
+
+};
+
+// Get users in room
+function getUsersInRoom() {
+
+};
